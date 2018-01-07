6217b24a1b44c2c125698f9426e6c2e4d46452c4
Janusz <janusz.czornik@templeofcode.com>
2018-01-07 19:20:22 +0100

    POST added to /json site

--- jsonHandle.py
+++ jsonHandle.py
@@ -1,5 +1,5 @@
 import json
-from flask import jsonify
+from flask import jsonify, request
 
 
 def create_simple_json_hello():
@@ -9,3 +9,24 @@ def create_simple_json_hello():
 
     json_data = json.dumps(data)
     return jsonify(json_data)
+
+
+def handle_json_post(posted_data: request):
+    if not posted_data.get_json():
+        return create_error_json()
+
+    else:
+        print(posted_data.get_json())
+        return create_ok_json()
+
+
+def create_error_json():
+    data = {}
+    data['error'] = "Did not received json!"
+    return jsonify(data)
+
+
+def create_ok_json():
+    data = {}
+    data['response'] = "OK"
+    return jsonify(data)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from jsonHandle import create_simple_json_hello
+from flask import Flask, render_template, request
+from jsonHandle import create_simple_json_hello, handle_json_post
 
 app = Flask(__name__)
 
@@ -9,10 +9,15 @@ def hello_world():
     return render_template('home.html')
 
 
-@app.route('/json')
-def hello_json():
+@app.route('/json', methods=['GET'])
+def hello_json_get():
     return create_simple_json_hello()
 
 
+@app.route('/json', methods=['POST'])
+def hello_json_post():
+    return handle_json_post(request)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
